c4df1a8a869eb2bb293243bc6701aedf6fa08ef3
Mateus Lucas <mateusslucass840@gmail.com>
2020-04-09 23:33:56 -0300

    meta tag color-theme

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const toggleSwitch = document.querySelector(
 	'.theme-switch input[type="checkbox"]'
 );
+
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme) {
@@ -13,9 +14,15 @@ if (currentTheme) {
 
 function switchTheme(element) {
 	if (element.target.checked) {
+		document
+			.querySelector('meta[name="theme-color"]')
+			.setAttribute("content", "#000");
 		document.documentElement.setAttribute("data-theme", "dark");
 		localStorage.setItem("theme", "dark");
 	} else {
+		document
+			.querySelector('meta[name="theme-color"]')
+			.setAttribute("content", "#FFF");
 		document.documentElement.setAttribute("data-theme", "light");
 		localStorage.setItem("theme", "light");
 	}
